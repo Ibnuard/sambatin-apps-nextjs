57b23f413700c8406a5299ce17611d8c7a83e0d3
Ibnu Putra <azxarblog@gmail.com>
2022-10-29 21:57:14 +0700

    initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout/layout'
+
+const FirstPost = () => {
+    return (
+        <Layout>
+            <Head>
+                <title>Sambatin Apps</title>
+            </Head>
+        </Layout>
+    )
+}
+
+export default FirstPost
